9255989a1bfb5eb4e0bbcb3fb62cae57d8176b0b
Sai H <72930376+saih1@users.noreply.github.com>
2023-08-07 15:29:16 +1000

    Migrate to Material3 and setup Theme 🐠

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,9 +1,12 @@
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -11,12 +14,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import common.RequestState
-import common.Status.*
+import common.Status.ERROR
+import common.Status.IDLE
+import common.Status.LOADING
+import common.Status.SUCCESS
 import data.remote.model.WeatherResponse
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
 import di.NetworkModule
 import presentation.WeatherViewModel
+import ui.theme.BasicBreezeTheme
 
 @Composable
 fun App() {
@@ -31,33 +38,39 @@ fun App() {
 
     val result: RequestState<WeatherResponse> by viewModel.weather.collectAsState()
 
-    MaterialTheme {
-        Column(
-            Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(5.dp)
+    BasicBreezeTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
         ) {
-            when (result.status) {
-                SUCCESS -> {
-                    Text(text = "City -> ${result.data?.location?.name ?: "null"}")
-                    Text(text = "Condition -> ${result.data?.current?.condition?.text ?: "null"}")
-                    Text(text = "Temperature -> ${result.data?.current?.tempC.toString()}")
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(5.dp),
+            ) {
+                Button(onClick = {
+                    viewModel.getWeather("Sydney, Australia")
+                }) {
+                    Text("Sydney")
                 }
-                ERROR -> Text(text = "ERROR")
-                IDLE -> Text(text = "IDLE")
-                LOADING -> Text(text = "LOADING")
-            }
 
-            Button(onClick = {
-                viewModel.getWeather("Sydney, Australia")
-            }) {
-                Text("Sydney")
-            }
-
-            Button(onClick = {
-                viewModel.getWeather("Melbourne, Australia")
-            }) {
-                Text("Melbourne")
+                Button(onClick = {
+                    viewModel.getWeather("Melbourne, Australia")
+                }) {
+                    Text("Melbourne")
+                }
+                when (result.status) {
+                    SUCCESS -> {
+                        Text(text = "City -> ${result.data?.location?.name ?: "null"}")
+                        Text(text = "Condition -> ${result.data?.current?.condition?.text ?: "null"}")
+                        Text(text = "Temperature -> ${result.data?.current?.tempC.toString()}")
+                    }
+                    ERROR -> Text(text = "ERROR")
+                    IDLE -> Text(text = "IDLE")
+                    LOADING -> Text(text = "LOADING")
+                }
             }
         }
     }
